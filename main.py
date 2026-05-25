197fe9f212992cc20fa60ab33456e1acb19e31d2
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-25 23:02:19 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterSarseWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterSarseWorker()
+    print("Worker engine initialized.")
